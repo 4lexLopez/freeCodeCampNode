e44d560aefe5fb16ae2b845fc42af361490984f9
4lexLopez <alex.roman.lopez@gmail.com>
2022-03-29 09:06:21 -0500

    item 6: .env file added

--- myApp.js
+++ myApp.js
@@ -1,5 +1,20 @@
 var express = require('express');
 var app = express();
+var absolutePath = __dirname + '/views/index.html';
+var jsonPath = __dirname + '/json';
+var publicPath = __dirname + '/public';
+// const mySecret = process.env['MESSAGE_STYLE'];
+
+// app.use('/public', express.static(publicPath));
+// app.get('/', function(req, res) {
+//   res.sendFile(absolutePath);
+// });
+
+app.get('/', function(req, res) {
+  var message = '';
+  process.env.MESSAGE_STYLE === 'uppercase' ? message = "HELLO JSON" : message = "Hello json";
+  res.json({"message": message});
+});
 
 
 
