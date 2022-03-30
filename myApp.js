003775114c1a846c84cd072b841ded0cfa8ebf21
4lexLopez <alex.roman.lopez@gmail.com>
2022-03-30 07:50:27 -0500

    returning to item 5 for testing purposes

--- myApp.js
+++ myApp.js
@@ -7,12 +7,12 @@ var jsonPath = __dirname + '/json';
 var publicPath = __dirname + '/public';
 // const mySecret = process.env['MESSAGE_STYLE'];
 
-// app.use('/public', express.static(publicPath));
-// app.get('/', function(req, res) {
-//   res.sendFile(absolutePath);
-// });
-console.log("config message: " + process.env.MESSAGE_STYLE);
+app.use('/public', express.static(publicPath));
 app.get('/', function(req, res) {
+  res.sendFile(absolutePath);
+});
+console.log("config message: " + process.env.MESSAGE_STYLE);
+app.get('/log', function(req, res) {
   var message = '';
   process.env.MESSAGE_STYLE === 'uppercase' ? message = "HELLO JSON" : message = "Hello json";
   res.json({"message": message});
